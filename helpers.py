85df7e3ed5a525224a61bb419d207e922d7ca408
Vicky Bikia <vickybikia@gmail.com>
2024-02-16 15:59:55 -0800

    Install dependencies in the build-and-test workflow and satisfy REUSE compliance specification

--- helpers.py
+++ helpers.py
@@ -1,3 +1,12 @@
+"""
+This source file is part of the Stanford Spezi open-source project.
+
+SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
+
+SPDX-License-Identifier: MIT
+
+"""
+
 import pandas as pd
 import numpy as np
 import csv

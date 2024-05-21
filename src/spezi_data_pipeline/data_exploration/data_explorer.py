3e20b15a62824667afc4a6e8cbb701cd5776c2cb
Vicky Bikia <vickybikia@gmail.com>
2024-05-21 14:43:18 -0700

    Add description of modules in README file and other updates

--- src/spezi_data_pipeline/data_exploration/data_explorer.py
+++ src/spezi_data_pipeline/data_exploration/data_explorer.py
@@ -600,12 +600,12 @@ def explore_total_records_number(  # pylint: disable=unused-variable
 
     counts = df.groupby(["LoincCode", "UserId"]).size().unstack(fill_value=0)
 
-    plt.figure(figsize=(20, 10))
+    plt.figure(figsize=(40, 50))
     counts.plot(kind="bar")
     plt.title("Number of records by Loinc code", fontsize=16)
     plt.xlabel("Loinc code", fontsize=14)
     plt.ylabel("Count", fontsize=14)
     plt.xticks(rotation=45, ha="right", fontsize=12)
-    plt.legend(title="User ID", fontsize=12, title_fontsize=14)
+    plt.legend(title="User ID", fontsize=12, title_fontsize=14, bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.tight_layout()
     plt.show()
